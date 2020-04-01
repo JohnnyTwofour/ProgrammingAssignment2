0917c8cc95b14acf159c0987633390413b2e4f6f
JohnnyTwofour <62763539+JohnnyTwofour@users.noreply.github.com>
2020-04-01 11:32:49 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Caches a matrix for use in a later function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##initialize x as a function argument
+  m <- NULL                                 ##initialized as an empty object to be used later in the code
+  
+  set <- function(y) {  ##takes argument y
+          x <<- y       ##the <<- assigns the y value to x in the parent environment (ie it can still be used once the set() function is complete)
+          m <<- NULL    ##assigns m as an empty object in the parent environment, this clears any values that may be residing in the m variable
+  }
+  get  <- function() x                          ##the symbol x is not defined so is retreived from the parent environment
+  setmatrix <- function(inverse) m <<- inverse  ##assigns the input matrix to the parent environment in the variable m
+  getmatrix <- function() m                     ##assigns the getter for variable m
+  
+  list(set = set, get = get,  ##assigns each element to a list and returns it to the parent environment,
+       setmatrix = setmatrix, ##naming the list elements allows for the use of $ to access functions once a matrix is stored in an element
+       getmatrix = getmatrix) 
 }
 
-
-## Write a short comment describing this function
+##Checks if the matrix is already inverted in variable m and, if FALSE, 
+##inverts the matrix from the makeCacheMatrix function output storing the result in m
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
+  
+## Return a matrix that is the inverse of 'x'
 }
